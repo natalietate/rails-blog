2f2580b805e9674dbfd8e508c192f57b1eb4a638
natalietate <martin.nat@gmail.com>
2017-11-20 14:59:09 -0500

    user controller, sign up form and path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   get 'welcome/index'
-
+  get '/signup', to: 'users#new'
   resources :posts do
     resources :comments
   end
+
+  resources :users
 
   root 'welcome#index'
 
