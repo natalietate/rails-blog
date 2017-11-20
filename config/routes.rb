f0dd0bf409a9f4283419f17274ffa2daa0e48ca7
natalietate <martin.nat@gmail.com>
2017-11-20 15:10:58 -0500

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'welcome/index'
+
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   resources :posts do
     resources :comments
   end
