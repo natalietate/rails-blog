04cef572105b23c4ab423f148d40eeae56dde04a
natalietate <martin.nat@gmail.com>
2017-11-20 14:14:18 -0500

    password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6 }
 end
