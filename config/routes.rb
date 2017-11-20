58c9bf9d52ae3d50bc06faa8ce5922c65f11d53c
Natalie Russo <martin.nat@gmail.com>
2017-11-20 15:14:01 -0500

    User signup (#2)
    * create users and add validation
    
    * add bcrypt and test validation
    
    * password validation
    
    * user controller, sign up form and path
    
    * finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   get 'welcome/index'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :posts do
     resources :comments
   end
+
+  resources :users
 
   root 'welcome#index'
 
