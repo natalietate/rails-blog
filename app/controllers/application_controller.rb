b1b727e9829e03935f0836c8c8dec86eab8b9f0c
natalietate <martin.nat@gmail.com>
2017-11-20 15:53:26 -0500

    add helper methods, update sessions controlelr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Log in the use
+  def sign_in
+    session[:user_id] = @user.id
+  end
+
+  # Returns the current user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Check if user is logged in
+  def logged_in?
+    !!current_user
+  end
+
+  helper_method :sign_in, :current_user, :logged_in?
 end
