ddda0eefa0fdabf36510a8fbc95449eb664919f6
natalietate <martin.nat@gmail.com>
2017-11-20 11:51:41 -0500

    added navigation, form validation and error messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def new
-
+    @post = Post.new
   end
 
   def index
@@ -13,8 +13,12 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
