306f841c7bae48b20e736d9b830df0a891e99c75
natalietate <martin.nat@gmail.com>
2017-11-21 17:27:05 -0500

    working seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(
+  name: 'Test',
+  email: 'test@test.com',
+  password: 'testing',
+  password_confirmation: 'testing'
+)
+
+User.create(
+  name: 'Natalie',
+  email: 'martin.nat@gmail.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+Post.create(
+  title: 'My very first post',
+  text: 'This is the text of my first post',
+  user_id: 1
+)
+Post.create(
+  title: 'Okay, here\'s a post',
+  text: 'More about the post!',
+  user_id: 2
+)
+Comment.create(
+  body: 'The comment I\'ve made',
+  post_id: 1,
+  user_id: 2
+)
