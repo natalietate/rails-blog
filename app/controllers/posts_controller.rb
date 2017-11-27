d801393fba326c04f373aa5c5d816f8f4e59bce0
natalietate <martin.nat@gmail.com>
2017-11-26 19:25:29 -0500

    final styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate, only: [:create, :destroy]
+  before_action :authenticate, only: [:index, :create, :destroy]
 
   def index
     @posts = Post.all
